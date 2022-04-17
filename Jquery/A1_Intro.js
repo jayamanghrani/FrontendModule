0349d512e9dae1ce2ac185b0f3c465ba7af37aac
Jaya Manghrani <Jaya.manghrani@ibm.com>
2022-04-17 18:34:14 +0530

    adding jquery code

---
+++ Jquery/A1_Intro.js
@@ -0,0 +1,35 @@
+//! What is jquery?
+
+/* jquery make it easier for us to use javascript on our website.
+
+jquery Js ki precoded library h, actuall me jquery javascript hi h ,
+javascript me ki jane vali activities ko simple krti h jquery.
+
+ex-
+
+we have this html-
+<div id="container">
+---
+---
+</div>
+
+To get the container in js--
+
+let a= document.querySelector("#container");
+or
+let b =document.getElementById("container");
+
+
+In jquery
+==========
+let c = $("#container")
+
+*/
+
+//! Jquery is primarily used for-
+/*
+-> DOM tree traversal( above ex is of this only---to reach to an specific element)
+->DOM tree manipulation (adding/removing new elements)
+->CSS effect (in javascript, we have to write ex div.style.background ="",, Jquery will simplify this)
+->Ajax req (in jquery only 1 line of code for ajax, while in javascript too many lines needed)
+*/
